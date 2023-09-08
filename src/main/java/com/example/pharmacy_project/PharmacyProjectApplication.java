1016da01502dc0fe3a343d2ec83773bcef45d0bd
Kyung-H <ekdeon@naver.com>
2023-09-08 22:42:46 +0900

    first commit

---
+++ src/main/java/com/example/pharmacy_project/PharmacyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pharmacy_project/PharmacyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
